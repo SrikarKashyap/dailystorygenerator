5516f89e5321835820897d83150f7b91d4c09ae2
devdess <sylvain.dessureault.1@ulaval.ca>
2018-11-23 16:16:52 -0500

    adding themes

--- dailystorygenerator/__main__.py
+++ dailystorygenerator/__main__.py
@@ -1,26 +1,41 @@
-from pathlib import Path
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import time
 import storygenerator
+import kivy
+from pathlib import Path
 
+kivy.require("1.9.0")
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
-
 from kivy.uix.popup import Popup
-
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.floatlayout import FloatLayout
 
 DATA_FOLDER = Path("img/")
+DEFAULT = "AVENGERS/"
+
+FICHIER_VILLE = "villes.txt"
+FICHIER_HEROES = "heroes_and_villains.txt"
+FICHIER_EVENTS = "events.txt"
+FICHIER_DEBUTS = "debuts.txt"
+DEFAULT_STORING_FILE = "story_content.txt"
+CONFIG_FILE = "config.txt"
 
 def file_to_open(nom_image):
     path_to_image = DATA_FOLDER / "{}.jpg".format(nom_image)
     return str(path_to_image)
 
+
 class BeginStoryPopUp(Popup):
     beginning = StringProperty()
+
     def __init__(self, **kwargs):
         super(BeginStoryPopUp, self).__init__(**kwargs)
         self.beginning = new_story.begininning
+
 
 class StoryScreenPopup(Popup):
     contents = StringProperty()
@@ -33,7 +48,7 @@ class StoryScreenPopup(Popup):
 
     def __init__(self, **kwargs):
         super(StoryScreenPopup, self).__init__(**kwargs)
-        with open("story_content.txt") as f:
+        with open(DEFAULT_STORING_FILE, "r") as f:
             self.contents = f.read()
             self.nom_hero = new_story.hero
             self.nom_villain = new_story.villain
@@ -46,184 +61,62 @@ class StoryScreenPopup(Popup):
         the_popup = BeginStoryPopUp()
         the_popup.open()
 
+
+class IntroScreen(Screen):
+
+    def assign_choice(self, choice):
+        new_story.load(choice+"/"+FICHIER_VILLE, choice+"/"+FICHIER_HEROES, choice+"/"+FICHIER_EVENTS,
+                       choice+"/"+FICHIER_DEBUTS, choice)
+
+    def spinner_clicked(self, value):
+        self.assign_choice(value)
+        with open(CONFIG_FILE, "w") as f:
+            f.write(value)
+
 class WelcomeScreen(Screen):
-    img_src = "avengers.jpg"
+    img_bkg = "WELCOME.jpg"
 
     def random_hero(self):
         new_story.random_hero()
-        new_story.store_info()
+
 
 class ScrollingScreen(Screen):
-    img_src = StringProperty(None)
+    img_hero = StringProperty(None)
     img_villain = StringProperty(None)
 
     def random_hero(self):
         new_story.random_hero()
-        new_story.store_info()
+
 
 class MyScreenManager(ScreenManager):
 
     def new_hero(self):
         new_story.increase_scroller()
         scroller = new_story.scroller % len(new_story.all_heroes)
-        img_src = file_to_open(new_story.all_heroes[scroller])
+        img_hero = file_to_open(new_story.all_heroes[scroller])
         img_villain = file_to_open(new_story.all_villains[scroller])
         name = str(time.time())  # getting unique name
-        s = ScrollingScreen(name=name)  # getting random colours and same alpha
+        s = ScrollingScreen(name=name)
         self.add_widget(s)
         self.current = name
-        s.img_src = img_src
+        s.img_hero = img_hero
         s.img_villain = img_villain
         new_story.new_hero(new_story.all_heroes[scroller])
-        new_story.store_info()
 
     def open_story_screen_popup(self):
         the_popup = StoryScreenPopup()
         the_popup.open()
 
 
-root_widget = Builder.load_string('''
-MyScreenManager:
-    WelcomeScreen:
-    
-<BeginStoryPopUp>:
-    title: "Debut:"
-    BoxLayout:
-        orientation: 'vertical'
-        Label:
-            text: "Debut de l'histoire..."
-            font_size: 30
-        ScrollView:
-            size_hint: 1, 1
-            TextInput:
-                font_size:20
-                height: self.minimum_height
-                color: 1,1,1,.5
-                size_hint_y: None
-                readonly: True
-                text: root.beginning
-        Button:
-            text: "Fermer"
-            on_press: root.dismiss()
-            font_size: 30
+buildKV = Builder.load_file("screenmanager.kv")
 
-<StoryScreenPopUp>:
-    title: "Votre Histoire de la Journee:"
-    BoxLayout:
-        orientation: 'vertical'
-        BoxLayout:
-            orientation: 'horizontal'
-            Label:
-                text: root.nom_hero
-                font_size: 20
-            Label:
-                font_size: 20
-            Label:
-                text: root.nom_villain
-                font_size: 20
-        BoxLayout:
-            orientation: 'horizontal'
-            Image:
-                source: root.img_hero
-                allow_stretch: False
-                keep_ratio: True
-            Image:
-                source: root.img_vs
-                allow_stretch: False
-                keep_ratio: True                
-            Image:
-                source: root.img_villain
-                allow_stretch: False
-                keep_ratio: True          
-        Label:
-            text: "L'histoire se passe a "+root.nom_ville
-            font_size: 30
-        Label:
-            text: "Et il y aura: "
-            font_size: 30
-        Label:
-            text: root.contents
-            font_size: 20
-            
-        Button:
-            text: "Commencer l'histoire"
-            on_press: root.open_begin_story_popup()
-            font_size: 30
-        Button:
-            text: "Fermer"
-            on_press: root.dismiss()
-
-        
-<WelcomeScreen>:
-    name: 'welcome'
-    BoxLayout:
-        orientation: 'vertical'
-        Label:
-            text: "Generateur d'Histoire!"
-            font_size: 30
-        Image:
-            source: root.img_src
-            allow_stretch: False
-            keep_ratio: True
-        BoxLayout:
-            Button:
-                text: 'Choisir un Hero au Hasard'
-                font_size: 30
-                on_press: root.random_hero()
-                on_release: app.root.open_story_screen_popup()
-            Button:
-                text: 'Choisir Mon Hero'
-                font_size: 30
-                on_release: app.root.new_hero()
-
-<ScrollingScreen>:
-    BoxLayout:
-        orientation: 'vertical'
-        BoxLayout:
-            orientation: 'horizontal'
-            Label:
-                text: "Hero"
-                font_size: 30
-            Label:
-                text: "Ennemi"
-                font_size: 30
-        BoxLayout:
-            orientation: 'horizontal'
-            Image:
-                source: root.img_src
-                allow_stretch: False
-                keep_ratio: True
-            Image:
-                source: root.img_villain
-                allow_stretch: False
-                keep_ratio: True
-        BoxLayout:
-            Button:
-                size_hint: (.5, .5)
-                text: 'OUI'
-                font_size: 30
-                on_release: app.root.open_story_screen_popup()
-            Button:
-                size_hint: (.5, .5)
-                text: 'NON'
-                font_size: 30
-                on_release: app.root.new_hero()
-            Button:
-                size_hint: (.5, .5)
-                text: 'Au Hasard'
-                font_size: 30
-                on_press: root.random_hero()
-                on_release: app.root.open_story_screen_popup()
-
-''')
-
-
-class ScreenManagerApp(App):
+class StoryGeneratorApp(App):
 
     def build(self):
-        return root_widget
+        return buildKV
 
 
 if __name__ == '__main__':
-    new_story = storygenerator.Storygenerator("villes.txt","heroes_and_villains.txt", "events.txt", "debuts.txt")
-    ScreenManagerApp().run()
+    new_story = storygenerator.Storygenerator(DEFAULT+FICHIER_VILLE, DEFAULT+FICHIER_HEROES,
+                                              DEFAULT+FICHIER_EVENTS, DEFAULT+FICHIER_DEBUTS)
+    StoryGeneratorApp().run()
--- dailystorygenerator/storygenerator.py
+++ dailystorygenerator/storygenerator.py
@@ -1,5 +1,11 @@
 import random
 
+DEFAULT_STORING_FILE = "story_content.txt"
+DEFAULT_THEME = "AVENGERS/"
+#DEFAULT_THEME = "HARRY POTTER/"
+#DEFAULT_THEME = "PRINCESSES/"
+#DEFAULT_THEME = "SHOWBIZ/"
+
 def from_file_to_dict(filename):
     content_as_dict = {}
     with open(filename, "r") as f:
@@ -16,32 +22,39 @@ def shuffle_and_get(a_list, num):
 class Storygenerator:
 
     def __init__(self, villes_file, heroes_file, events_file, debuts_file):
+        self.load(villes_file, heroes_file, events_file, debuts_file, DEFAULT_THEME)
+
+    def load(self, villes_file, heroes_file, events_file, debuts_file, theme_choisi):
+        self.villes_file, self.heroes_file, self.events_file, self.debuts_file = [villes_file,
+                                                                                  heroes_file,
+                                                                                  events_file,
+                                                                                  debuts_file]
+        self.theme = theme_choisi
         self.scroller = 0
-        self.hero = None
-        self.villain = None
-        with open(villes_file, "r") as f:
+        with open(self.villes_file, "r") as f:
             villes = f.read().splitlines()
         self.ville = shuffle_and_get(villes, 1)[0]
-        self.all_heroes_and_villains = from_file_to_dict(heroes_file)
-        self.all_beginnings = from_file_to_dict(debuts_file)
-        self.begininning = self.all_beginnings[self.ville]
+        self.all_heroes_and_villains = from_file_to_dict(self.heroes_file)
         self.all_heroes = list(self.all_heroes_and_villains.keys())
         self.all_villains = list(self.all_heroes_and_villains.values())
-        with open(events_file, "r") as f:
+        self.all_beginnings = from_file_to_dict(self.debuts_file)
+        self.begininning = self.all_beginnings[self.ville]
+        with open(self.events_file, "r") as f:
             events = f.read().splitlines()
         self.events = shuffle_and_get(events, 3)
-        self.store_info()
+        self.store_events(DEFAULT_STORING_FILE)
+        self.store_config(self.theme)
 
     def random_hero(self):
         choix = random.randint(0, len(self.all_heroes_and_villains) - 1)
         self.hero = list(self.all_heroes_and_villains.keys())[choix]
         self.villain = self.all_heroes_and_villains[self.hero]
-        self.store_info()
+        self.store_events(DEFAULT_STORING_FILE)
 
     def new_hero(self, choice):
         self.hero = choice
         self.villain = self.all_heroes_and_villains[self.hero]
-        self.store_info()
+        self.store_events(DEFAULT_STORING_FILE)
 
     def increase_scroller(self):
         self.scroller += 1
@@ -49,21 +62,24 @@ class Storygenerator:
     def changer_jour(self, jour):
         self.jour = jour
 
-    def store_info(self):
-        with open("story_content.txt", "w") as f:
-#            f.write("Hero: {}\nVillain: {}\nVille: {}".format(self.hero, self.villain, self.ville))
-#            f.write(self.ville+"\n\n")
-#            f.write("\nEvenements: \n")
+    def changer_background(self, new_bkground):
+        self.background = new_bkground+".jpg"
+
+    def store_events(self, store_file):
+        with open(store_file, "w") as f:
             for i in range(len(self.events)-1):
                 f.write(self.events[i]+", ")
             f.write(self.events[len(self.events)-1])
+
+    def store_config(self, item=DEFAULT_THEME):
+        with open("config.txt", "w") as f:
+            f.write(item)
 
     def __str__(self):
         return "Hero: {}\nVillain: {}\nVille: {}\nEvenements: {}".format(self.hero,
                                                                   self.villain,
                                                                   self.ville,
                                                                   self.events)
-
     def __repr__(self):
         return str(self)
 
